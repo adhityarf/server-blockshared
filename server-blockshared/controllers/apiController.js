2b9f658d6cef550277b7c644f72d30b98322624e
Piperipheral <Piperipheral@Gmail.com>
2020-08-14 18:03:56 +0700

    minor changes

--- server-blockshared/controllers/apiController.js
+++ server-blockshared/controllers/apiController.js
@@ -1,17 +1,16 @@
-require("dotenv").config()
+require("dotenv").config();
 
-const bcrypt = require("bcryptjs")
-const path = require('path')
+const bcrypt = require("bcryptjs");
+const path = require("path");
 
-const jwt = require('jsonwebtoken')
+const jwt = require("jsonwebtoken");
 
-
-const Asset = require('../models/Assets')
-const Transaction = require('../models/Transaction')
-const Cust = require('../models/Custs')
-const Token = require("../models/Token")
-const { token } = require("morgan")
-const { deleteOne } = require("../models/Assets")
+const Asset = require("../models/Assets");
+const Transaction = require("../models/Transaction");
+const Cust = require("../models/Custs");
+const Token = require("../models/Token");
+const { token } = require("morgan");
+const { deleteOne } = require("../models/Assets");
 
 module.exports = {
   // NEW BLOCKSHARED
@@ -21,48 +20,55 @@ module.exports = {
       const cust = await Cust.create({
         email,
         password,
-        fullName
+        fullName,
       });
       res.status(200).json({
         message: "Signup Success",
-        status: true
-      })
+        status: true,
+      });
     } catch (error) {
       res.status(404).json({
         message: "Signup Failed" + error,
-        status: false
-      })
+        status: false,
+      });
     }
   },
   signinCust: async (req, res) => {
     try {
       const { email, password } = req.body;
-      const cust = await Cust.findOne({ email: email })
+      const cust = await Cust.findOne({ email: email });
       if (!cust) {
         res.status(404).json({
           message: "User Not Found !",
-          status: false
-        })
+          status: false,
+        });
       }
       const isPasswordMatch = await bcrypt.compare(password, cust.password);
       if (!isPasswordMatch) {
         res.status(404).json({
           message: "Password incorrect !",
-          status: false
-        })
+          status: false,
+        });
       }
       const custObj = {
         custId: cust.id,
-        custEmail: cust.email
-      }
-      const accessToken = jwt.sign(custObj, process.env.REACT_APP_ACCESS_TOKEN_SECRET, { expiresIn: '30s' });
-      const refreshToken = jwt.sign(custObj, process.env.REACT_APP_REFRESH_TOKEN_SECRET)
+        custEmail: cust.email,
+      };
+      const accessToken = jwt.sign(
+        custObj,
+        process.env.REACT_APP_ACCESS_TOKEN_SECRET,
+        { expiresIn: "10s" }
+      );
+      const refreshToken = jwt.sign(
+        custObj,
+        process.env.REACT_APP_REFRESH_TOKEN_SECRET
+      );
 
       // INSERT REFRESH TOKEN TO DB
       const token = await Token.create({
         custId: cust.id,
         custEmail: cust.email,
-        refreshToken: refreshToken
+        refreshToken: refreshToken,
       });
 
       res.status(200).json({
@@ -70,32 +76,31 @@ module.exports = {
         status: true,
         accessToken: accessToken,
         refreshToken: refreshToken,
-        token
-      })
+        token,
+      });
     } catch (error) {
       res.status(404).json({
         message: "Login Unsuccessful",
-        status: false
-      })
+        status: false,
+      });
     }
   },
   signoutCust: async (req, res) => {
     req.session.destroy();
-
-
-    id = req.user.custId
-    const deleteRefreshToken = await Token.deleteOne({ custId: id })
+    id = req.user.custId;
+    console.log("bro: " + req.user.custId);
+    const deleteRefreshToken = await Token.deleteOne({ custId: id });
 
     if (!deleteRefreshToken) {
       return res.status(201).json({
-        message: "Failed to delete refresh token"
-      })
+        message: "Failed to delete refresh token",
+      });
     }
 
     res.status(204).json({
       message: "Logout Success",
-      status: true
-    })
+      status: true,
+    });
   },
   uploadAssets: async (req, res) => {
     try {
@@ -106,41 +111,39 @@ module.exports = {
           message: "Image Not Found" + error,
         });
       }
-      const extName = path.extname(`${req.file.filename}`)
+      const extName = path.extname(`${req.file.filename}`);
       const newAsset = {
         oriFile: {
           fileName: `${req.file.filename}`,
           fileSize: `${req.file.size}`,
           fileUrl: `images/${req.file.filename}`,
-          fileType: extName
+          fileType: extName,
         },
         blockFile: {
           dataHash: "dataHash",
           signee: cust.fullName,
-          signeeAddress: "signeeAddress"
+          signeeAddress: "signeeAddress",
         },
         owner: cust._id,
-      }
+      };
       const asset = await Asset.create(newAsset);
       cust.assetsId.push({ _id: asset._id });
       await cust.save();
       res.status(200).json({
         asset,
         message: "Upload Asset Success",
-        status: true
-      })
+        status: true,
+      });
     } catch (error) {
       res.status(404).json({
         message: "Upload Asset Failed" + error,
-        status: false
-      })
+        status: false,
+      });
     }
   },
   topUp: async (req, res) => {
     try {
-      const {
-        nominal, bankFrom, accountHolder, code, email, bp
-      } = req.body;
+      const { nominal, bankFrom, accountHolder, code, email, bp } = req.body;
       const cust = await Cust.findOne({ email: email });
       if (!req.file) {
         return res.status(404).json({ message: "Image Not Found" });
@@ -159,85 +162,94 @@ module.exports = {
         payments: {
           proofPayment: `images/${req.file.filename}`,
           bankFrom: bankFrom,
-          accountHolder: accountHolder
+          accountHolder: accountHolder,
         },
-      }
+      };
 
-      const topUp = await Transaction.create(newTopUp)
+      const topUp = await Transaction.create(newTopUp);
 
       res.status(201).json({
         message: "Payment Success",
         topUp,
-        status: true
-      })
-
+        status: true,
+      });
     } catch (error) {
       res.status(500).json({
         message: "Internal Server Error" + error,
-        status: false
-      })
+        status: false,
+      });
     }
   },
 
   // VIEW CUST BLOCKSHARED
   dashboardPage: async (req, res) => {
     try {
-      const id = req.user.custId
-      console.log(req.user)
-      const asset = await Asset.findOne({ owner: id })
-        .populate({ path: 'owner' })
+      const id = req.user.custId;
+      console.log(req.user);
+      const asset = await Asset.findOne({ owner: id }).populate({
+        path: "owner",
+      });
       res.status(200).json({
         asset,
-        id: req.user.id
-      })
+        id: req.user.id,
+      });
     } catch (error) {
-      res.status(404).json({ message: "Couldn't find any data !" } + error)
+      res.status(404).json({ message: "Couldn't find any data !" } + error);
     }
   },
   credentialPage: async (req, res) => {
     try {
-      const id = req.session.custSess.id
-      const asset = await Asset.findOne({ owner: id })
-        .populate({ path: 'owner' })
+      const id = req.session.custSess.id;
+      const asset = await Asset.findOne({ owner: id }).populate({
+        path: "owner",
+      });
       res.status(200).json({
         asset,
-        id: req.user.id
-      })
+        id: req.user.id,
+      });
     } catch (error) {
-      res.status(404).json({ message: "Couldn't find any data !" } + error)
+      res.status(404).json({ message: "Couldn't find any data !" } + error);
     }
   },
 
   // JWT TOKEN
   refreshToken: async (req, res) => {
-    const refreshTokenBody = req.body.token
-    const email = req.body.email
-    const tokendb = await Token.findOne({ refreshToken: refreshTokenBody })
+    const refreshTokenBody = req.body.token;
+    const email = req.body.email;
+    const tokendb = await Token.findOne({ refreshToken: refreshTokenBody });
+    console.log(email);
 
-    console.log(tokendb)
+    console.log(tokendb);
 
-    if (tokendb === null) return res.sendStatus(403)
-    if (refreshTokenBody === null) return res.sendStatus(401)
-    if (email != tokendb.custEmail) return res.sendStatus(403)
+    if (tokendb === null) return res.sendStatus(500);
+    if (refreshTokenBody === null) return res.sendStatus(401);
+    if (email != tokendb.custEmail) return res.sendStatus(403);
 
     const custObj = {
       custId: tokendb.custId,
-      custEmail: tokendb.custemail
-    }
+      custEmail: tokendb.custemail,
+    };
 
+    jwt.verify(
+      refreshTokenBody,
+      process.env.REACT_APP_REFRESH_TOKEN_SECRET,
+      (err, user) => {
+        if (err) return res.status(403).json({ err });
 
-    jwt.verify(refreshTokenBody, process.env.REACT_APP_REFRESH_TOKEN_SECRET, (err, user) => {
-      if (err) return res.status(403).json({ err })
+        const accessToken = jwt.sign(
+          custObj,
+          process.env.REACT_APP_ACCESS_TOKEN_SECRET,
+          { expiresIn: "10s" }
+        );
 
-      const accessToken = jwt.sign(custObj, process.env.REACT_APP_ACCESS_TOKEN_SECRET, { expiresIn: '30s' });
-
-      res.status(200).json({
-        message: "Refresh token success",
-        status: true,
-        accessToken: accessToken,
-        tokendb,
-        custObj
-      })
-    })
-  }
-}
+        res.status(200).json({
+          message: "Refresh token success",
+          status: true,
+          accessToken: accessToken,
+          tokendb,
+          custObj,
+        });
+      }
+    );
+  },
+};
--- server-blockshared/middlewares/auth.js
+++ server-blockshared/middlewares/auth.js
@@ -1,8 +1,8 @@
 require("dotenv").config();
-const jwt = require('jsonwebtoken')
+const jwt = require("jsonwebtoken");
 
 const auth = async (req, res, next) => {
-  const token = req.header("X-Access-Token");
+  const token = req.header("authorization");
   if (!token) {
     return res.status(401).json({
       code: "BAD_REQUEST_ERROR",
@@ -11,16 +11,20 @@ const auth = async (req, res, next) => {
   }
 
   try {
-    jwt.verify(token, process.env.REACT_APP_ACCESS_TOKEN_SECRET, (err, user) => {
-      if (err) {
-        return res.status(403).json({
-          code: "FORBIDDEN",
-          description: "Forbidden access",
-        });
+    jwt.verify(
+      token,
+      process.env.REACT_APP_ACCESS_TOKEN_SECRET,
+      (err, user) => {
+        if (err) {
+          return res.status(403).json({
+            code: "FORBIDDEN",
+            description: "Forbidden access",
+          });
+        }
+        req.user = user;
+        next();
       }
-      req.user = user;
-      next();
-    });
+    );
   } catch (error) {
     return res.status(500).json({
       code: "SERVER_ERROR",
