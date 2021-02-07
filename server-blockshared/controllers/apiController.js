34513203c85ede30dcf587970f30efb9c4f26e8f
Adhitya <adhityarizal.firdaus@yahoo.com>
2021-02-07 20:14:57 +0700

    add header 3

--- server-blockshared/controllers/apiController.js
+++ server-blockshared/controllers/apiController.js
@@ -151,7 +151,7 @@ module.exports = {
         const asset = await Asset.create(newAsset);
         cust.assetsId.push({ _id: asset._id });
         await cust.save();
-        res.set('Access-Control-Allow-Origin', '*');
+        res.set('Access-Control-Allow-Origin', 'http://www.blockshared.id');
         res.removeHeader("Content-Type");
         res.status(200).json({
           asset,
